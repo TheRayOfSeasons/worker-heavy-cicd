b2791c20f214cc40a3594e86c6e03e45da8df594
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2021-02-13 12:36:50 +0800

    [Modify] Query flavor values from DB

--- app/worker_service/ice_creams/reports.py
+++ app/worker_service/ice_creams/reports.py
@@ -1,3 +1,7 @@
+from pypika import PostgreSQLQuery
+from pypika import Table
+
+from core.database import CONNECTION
 from core.sheets import ExcelReport
 
 
@@ -14,7 +18,15 @@ class FlavorsReport(ExcelReport):
             'text': 'Description'
         }
     ]
-    row_data = [
-        ['Rocky Road', 'WOW'],
-        ['Cookies and Cream', 'XD']
-    ]
+
+    def get_row_data(self, *args, **kwargs):
+        row_data = super().get_row_data(*args, **kwargs)
+        flavors = Table('ice_creams_flavors')
+        query = (
+            PostgreSQLQuery
+            .from_(flavors)
+            .select('name', 'description')
+        )
+        result = CONNECTION.execute_query(query)
+        row_data.extend(result)
+        return row_data
